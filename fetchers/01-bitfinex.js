8d37ec1b9e1845da5299ec41fbbe6b66b641f285
decodedbrain <decodemybrain@protonmail.ch>
2017-10-29 14:15:17 +0300

    Delete unused code

--- fetchers/01-bitfinex.js
+++ fetchers/01-bitfinex.js
@@ -1,12 +1,10 @@
 const 
 	got = require('got');
-	config = require('config'),
 	fs = require('fs');
 	
 async function fetch(){
 	const URL = 'https://api.bitfinex.com/v1/pubticker/ethusd'
 	response = await got(URL,{json:true}).body;
-	console.log(response);
 	return {
 		"mid":response.mid, 
 		"low":response.low,
