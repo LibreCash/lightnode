98f9075cf6b175d66f8d968559d3729315ce7407
neo-crypt <neo-crypt@neo-crypt.ai>
2017-11-04 19:58:25 +0200

    light/master node api/logic, liking & debug in progress

--- test/test-net.js
+++ test/test-net.js
@@ -1,21 +1,47 @@
 const 
     client = require('../lib/net/client'),
-    server = require('../lib/net/server');
+    server = require('../lib/net/server'),
+    LightNode = require('../lib/node/lightnode'),
+    MasterNode = require('../lib/node/masternode');
 
 var options = require('../lib/net/config.js');
 
-server.start(options);
+async function run() {
+    server.start(options, async ()=>{
+        await client.connect(options);
+        console.log('aaa');
+        
+        var clientState = await client.getClientState();
+        console.log('clientState', clientState);
+        
+        var nodeState = await client.getNodeState();
+        console.log('nodeState', nodeState);
+        
+        var rates = await client.getRates();
+        console.log('rates', rates);
+        
+        var notifications = await client.getNotifications();
+        console.log('notifications', notifications);
+    });
+}
 
-client.connect(options);
+//run();
 
-var clientState = client.getClientState();
-console.log(clientState);
 
-var nodeState = client.getNodeState();
-console.log(nodeState);
+const lightNode = new LightNode();
 
-var rates = client.getRates();
-console.log(rates);
+lightNode.start(options);
 
-var notifications = client.getNotifications();
-console.log(notifications);
+const masterNode = new MasterNode();
+
+masterNode.on('finished', () => {
+    logger.info('master node finish');
+});
+masterNode.on('nodeConnected', () => {
+    logger.info('node connected');
+});
+masterNode.on('nodeDisconnected', () => {
+    logger.info('node disconnected');
+});
+
+masterNode.addNode(options);
