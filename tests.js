c4a9149d09699392816cd4481d27e6a855cfda29
neo-crypt <neo-crypt@neo-crypt.ai>
2018-01-22 20:58:58 -0500

    exchange onoff fix

--- tests.js
+++ tests.js
@@ -1,5 +1,4 @@
 //require('./test/fetch-test')
-//require('./test/test-mongo')
 //require('./lib/net/apigen').generate();
 require('./test/test-net');
 //require('./cli/lightnode-cli'); //only for debug!
