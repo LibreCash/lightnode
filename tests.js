ab94c10876ea79e35bb31830f6f0263209f54560
neo-crypt <neo-crypt@neo-crypt.ai>
2018-02-11 15:55:42 -0500

    fix auth & cleanup

--- tests.js
+++ tests.js
@@ -1,6 +1,3 @@
-//require('./test/fetch-test')
 //require('./lib/net/apigen').generate();
 require('./test/test-net');
-//require('./cli/lightnode-cli'); //only for debug!
-//require('./cli/masternode-cli'); //only for debug!
 
